824a38a4f0be8a2613962cd9be67abfba2d88fbe
Jishnu T V <jishnuprgm@outlook.com>
2021-04-08 14:40:23 +0530

    Auth register changed

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router();
 
-router.post("/register", register);
+// router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/reauth", reAuth);
