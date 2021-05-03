3d04dd3d923f456a68a9970304aa14552208b95c
Jishnu T V <jishnuprgm@outlook.com>
2021-05-03 16:50:47 +0530

    Student Attandance

--- routes/student.js
+++ routes/student.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
     getStudent,
     getStudentCourse,
-    getAttandance
+    getAttendance
 } = require('../controllers/student');
 
 const router = express.Router();
